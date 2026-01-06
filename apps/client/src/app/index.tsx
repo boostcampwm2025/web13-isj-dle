575475ef6e989b6dc41f66849cbbbcdfa9e1e4fe
Lims <swoo0514lim@gmail.com>
2026-01-07 00:23:38 +0900

    feat: #49 useWebSocket 커스텀 훅 생성

--- apps/client/src/app/index.tsx
+++ apps/client/src/app/index.tsx
@@ -1,8 +1,17 @@
+import { TestWebSocket } from "./TestWebSocket";
 import { PhaserProvider } from "./providers/PhaserProvider";
+import { WebSocketProvider } from "./providers/WebSocketProvider";
 import "./styles/index.css";
 
 function App() {
-  return <PhaserProvider>{/* 향후 UI 레이어 추가 (HUD, Menu 등) */}</PhaserProvider>;
+  return (
+    <WebSocketProvider>
+      <PhaserProvider>
+        <TestWebSocket />
+        {/* 향후 UI 레이어 추가 (HUD, Menu 등) */}
+      </PhaserProvider>
+    </WebSocketProvider>
+  );
 }
 
 export default App;
--- apps/client/src/app/providers/WebSocketProvider.tsx
+++ apps/client/src/app/providers/WebSocketProvider.tsx
@@ -1,4 +1,4 @@
-import { WebSocketContext } from "./websocket-context";
+import { WebSocketContext } from "./use-websocket";
 import { Socket, io } from "socket.io-client";
 
 import { type ReactNode, useEffect, useRef, useState } from "react";
