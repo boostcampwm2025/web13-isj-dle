57d23eab0be3430e5a96227b78babfcbda808f04
Lims <swoo0514lim@gmail.com>
2026-01-07 00:24:37 +0900

    chore: test component 삭제

--- apps/client/src/app/index.tsx
+++ apps/client/src/app/index.tsx
@@ -1,4 +1,3 @@
-import { TestWebSocket } from "./TestWebSocket";
 import { PhaserProvider } from "./providers/PhaserProvider";
 import { WebSocketProvider } from "./providers/WebSocketProvider";
 import "./styles/index.css";
@@ -6,10 +5,7 @@ import "./styles/index.css";
 function App() {
   return (
     <WebSocketProvider>
-      <PhaserProvider>
-        <TestWebSocket />
-        {/* 향후 UI 레이어 추가 (HUD, Menu 등) */}
-      </PhaserProvider>
+      <PhaserProvider>{/* 향후 UI 레이어 추가 (HUD, Menu 등) */}</PhaserProvider>
     </WebSocketProvider>
   );
 }
