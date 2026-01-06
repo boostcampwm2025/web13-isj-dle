aa3ba3149d67237dd27108c88097ef1ff6d825e7
MinChanJu <mcj00220077@gmail.com>
2026-01-07 00:52:36 +0900

    feat: #42 사이드바 프로바이더 및 레이아웃 추가

--- apps/client/src/app/index.tsx
+++ apps/client/src/app/index.tsx
@@ -1,8 +1,17 @@
+import Sidebar from "../widgets/Sidebar";
+import { SidebarProvider } from "../widgets/Sidebar/model/SidebarProvider";
 import { PhaserProvider } from "./providers/PhaserProvider";
 import "./styles/index.css";
 
 function App() {
-  return <PhaserProvider>{/* 향후 UI 레이어 추가 (HUD, Menu 등) */}</PhaserProvider>;
+  return (
+    <PhaserProvider>
+      <SidebarProvider>
+        <Sidebar />
+        {/* UI 레이어 추가 (HUD, Menu 등) */}
+      </SidebarProvider>
+    </PhaserProvider>
+  );
 }
 
 export default App;
