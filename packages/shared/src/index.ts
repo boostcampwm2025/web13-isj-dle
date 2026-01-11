84fdd2da28e069dabed558ac27c46639b83575d8
eunji <pih9134@naver.com>
2026-01-11 19:56:16 +0900

    feat: #13 room-event.types.ts 파일 추가 및 내보내기

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,3 +4,4 @@ export * from "./notice.types";
 export * from "./user.types";
 export * from "./game-user.types";
 export * from "./room.types";
+export * from "./room-event.types";
---
+++ packages/shared/src/room-event.types.ts
@@ -0,0 +1,15 @@
+export enum RoomEventType {
+  ROOM_JOIN = "room:join",
+  ROOM_JOINED = "room:joined",
+}
+
+export interface RoomJoinPayload {
+  roomId: string;
+}
+
+export interface RoomJoinedPayload {
+  userId: string;
+  roomId: string;
+  nickname: string;
+  users: Array<{ id: string; nickname: string }>;
+}
