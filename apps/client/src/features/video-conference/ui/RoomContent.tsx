457928f59741326e7373dd4ccf7adfaeeebbeb1c
MinChanJu <mcj00220077@gmail.com>
2026-01-13 19:22:18 +0900

    feat: #82 화면 공유 기능을 위한 의존성 주입

--- apps/client/src/features/video-conference/ui/RoomContent.tsx
+++ apps/client/src/features/video-conference/ui/RoomContent.tsx
@@ -21,12 +21,16 @@ const RoomContent = () => {
   useEffect(() => {
     const { setLocalParticipant: setMicLocalParticipant } = getHookByKey("mic");
     const { setLocalParticipant: setCameraLocalParticipant } = getHookByKey("camera");
+    const { setLocalParticipant: setScreenShareLocalParticipant } = getHookByKey("screen_share");
+
     setMicLocalParticipant?.(localParticipant);
     setCameraLocalParticipant?.(localParticipant);
+    setScreenShareLocalParticipant?.(localParticipant);
 
     return () => {
       setMicLocalParticipant?.(null);
       setCameraLocalParticipant?.(null);
+      setScreenShareLocalParticipant?.(null);
     };
   }, [getHookByKey, localParticipant]);
 
