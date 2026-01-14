2b2bbcef46659ddec30cd8161ad69b183286b0c4
김민영 <rlaalsdud41@naver.com>
2026-01-14 11:23:06 +0900

    refactor: #77 VideoRoomWidget에 LiveKitRoomWrapper 통합
    - VideoRoom에서 ProximityThumbnailList 제거
    - PhaserLayout에서 VideoRoom 제거
    - VideoRoomWidget에서 LiveKitRoomWrapper로 감싸도록 변경
    - lobby에서 contactId 기반 thumbnail 모드 지원

--- apps/client/src/features/video-conference/ui/VideoRoom.tsx
+++ apps/client/src/features/video-conference/ui/VideoRoom.tsx
@@ -1,13 +1,9 @@
-import ProximityThumbnailList from "./ProximityThumbnailList";
 import RoomContent from "./RoomContent";
 import RoomInfo from "./RoomInfo";
 
 const VideoRoom = () => {
   return (
     <div className="fixed inset-0 z-5 bg-black/50">
-      <div className="fixed top-0 right-0 left-0 z-50 bg-black/70">
-        <ProximityThumbnailList />
-      </div>
       <div className="absolute top-20 left-4 z-10 flex gap-2">
         <RoomInfo />
       </div>
