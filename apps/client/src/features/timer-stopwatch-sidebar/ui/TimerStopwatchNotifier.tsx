ade225e61d36bdba25d7d95f23f05e569d52d419
김민영 <rlaalsdud41@naver.com>
2026-01-21 23:10:50 +0900

    fix: #142 useSyncTimer를 useTimerActions와 분리
    - 사이드 탭에 타이머 프로그레스바가 안 뜨는 문제
    - useSyncTimer를 useTimerActions와 분리해 따로 주입

--- apps/client/src/features/timer-stopwatch-sidebar/ui/TimerStopwatchNotifier.tsx
+++ apps/client/src/features/timer-stopwatch-sidebar/ui/TimerStopwatchNotifier.tsx
@@ -1,6 +1,14 @@
 import { useTimerCompletionNotification } from "../model";
+import { useSyncTimer } from "../model/use-sync-timer";
+
+import { useUserStore } from "@entities/user";
 
 export const TimerStopwatchNotifier = () => {
+  const roomId = useUserStore((state) => state.user?.avatar.currentRoomId ?? null);
+  const isMeetingRoom = roomId?.startsWith("meeting") ?? false;
+
+  useSyncTimer({ roomId, isMeetingRoom });
   useTimerCompletionNotification();
+
   return null;
 };
