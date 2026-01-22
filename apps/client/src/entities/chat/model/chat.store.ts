362251ca7ab2c9c579f237ad74499e71450ca99f
eunji <pih9134@naver.com>
2026-01-22 10:53:59 +0900

    feat: #168 unreadCount 상태 추가

--- apps/client/src/entities/chat/model/chat.store.ts
+++ apps/client/src/entities/chat/model/chat.store.ts
@@ -6,22 +6,28 @@ type ChatStore = {
   roomName: string | null;
   chatMessages: ReceivedChatMessage[];
   systemMessages: ReceivedChatMessage[];
+  unreadCount: number;
 
   reset: (roomName: string) => void;
   addSystemMessage: (msg: ReceivedChatMessage) => void;
   setChatMessages: (msgs: ReceivedChatMessage[]) => void;
+  incrementUnreadCount: () => void;
+  resetUnreadCount: () => void;
 };
 
 export const useChatStore = create<ChatStore>((set, get) => ({
   roomName: null,
   chatMessages: [],
   systemMessages: [],
+  unreadCount: 0,
 
-  reset: (roomName: string) => set({ roomName, chatMessages: [], systemMessages: [] }),
+  reset: (roomName: string) => set({ roomName, chatMessages: [], systemMessages: [], unreadCount: 0 }),
   addSystemMessage: (msg) => {
     const { systemMessages } = get();
     const next = [...systemMessages, msg];
     set({ systemMessages: next });
   },
   setChatMessages: (msgs) => set({ chatMessages: msgs }),
+  incrementUnreadCount: () => set((state) => ({ unreadCount: state.unreadCount + 1 })),
+  resetUnreadCount: () => set({ unreadCount: 0 }),
 }));
