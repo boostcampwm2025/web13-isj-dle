8cbe26dabe9af8dc96477bd0f4f94eab1465e478
MinChanJu <mcj00220077@gmail.com>
2026-02-03 17:39:50 +0900

    setting: #237 라우터 및 svg 설정 관련 패키지 업데이트

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
   server: {
     proxy: {
       "/api": {
