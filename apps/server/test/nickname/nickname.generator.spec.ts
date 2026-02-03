1c9691ccdbd194f39a8c092304e72609fb5cae41
eunji <pih9134@naver.com>
2026-02-03 23:08:45 +0900

    test: #249 nickname 생성 테스트 로직 수정
    - 형용사 + 공백 + 명사 + 공백 + 숫자 형식으로 닉네임 반환

--- apps/server/test/nickname/nickname.generator.spec.ts
+++ apps/server/test/nickname/nickname.generator.spec.ts
@@ -4,18 +4,18 @@ describe("generateUniqueNickname", () => {
   test("기본 생성: 중복이 없으면 숫자가 1이어야 함", () => {
     const nickname = generateUniqueNickname(() => false);
 
-    expect(nickname).toMatch(/^[가-힣]+1$/);
+    expect(nickname).toMatch(/^[가-힣]+ [가-힣]+ 1$/);
   });
 
   test("중복 처리: 1번이 중복이면 형용사+명사+2번이 나와야 함", () => {
     const nickname = generateUniqueNickname((name) => name.endsWith("1"));
 
-    expect(nickname).toMatch(/^[가-힣]+2$/);
+    expect(nickname).toMatch(/^[가-힣]+ [가-힣]+ 2$/);
   });
 
   test("연속 중복 처리: 1, 2, 3번이 중복이면 형용사+명사+4번이 나와야 함", () => {
     const nickname = generateUniqueNickname((name) => /([1-3])$/.test(name));
 
-    expect(nickname).toMatch(/^[가-힣]+4$/);
+    expect(nickname).toMatch(/^[가-힣]+ [가-힣]+ 4$/);
   });
 });
